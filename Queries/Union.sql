741f36a95f594de5c138ef8d022ff9233fc8073a
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2022-01-31 20:03:28 +0100

    union

--- Queries/Union.sql
+++ Queries/Union.sql
@@ -1,3 +1,62 @@
+-- MUCHO OJO CON LOS UNION !!!!!
+-- Haré un UNION cuando lo necesite.
+-- Listado de personas sin empresa   
+SELECT 
+  Personas.Id,
+  Personas.Nombre,
+  Personas.Apellidos
+FROM 
+  Personas
+WHERE 
+  Personas.EmpresaID IS NULL
+;
+-- Listado de personas con empresa y nombre de empresa
+SELECT 
+  Personas.Id,
+  Personas.Nombre,
+  Personas.Apellidos,
+  Empresas.Nombre
+FROM 
+  Personas
+  INNER JOIN Empresas ON Empresas.Id = Personas.EmpresaId
+;
+-- JUNTO LOS DOS CON UN UNION
+SELECT 
+  Personas.Id,
+  Personas.Nombre,
+  Personas.Apellidos,
+  Empresas.Nombre as Empresa
+FROM 
+  Personas
+  INNER JOIN Empresas ON Empresas.Id = Personas.EmpresaId
+UNION 
+SELECT 
+  Personas.Id,
+  Personas.Nombre,
+  Personas.Apellidos,
+  null AS EMPRESA
+FROM 
+  Personas
+WHERE 
+  Personas.EmpresaID IS NULL
+;
+-- Listado de personas con empresa y nombre de empresa si las tiene
+SELECT 
+  Personas.Id,
+  Personas.Nombre,
+  Personas.Apellidos,
+  Empresas.Nombre
+FROM 
+  Personas
+  LEFT JOIN Empresas ON Empresas.Id = Personas.EmpresaId
+;
+
+
+
+
+
+
+
 -- Nombre y DNI de las personas que no están en empresas... 
 -- y el importe total a facturar el año pasado
 SELECT
@@ -15,15 +74,13 @@ WHERE
 GROUP BY
   Nombre,
   NIF
-
 UNION
-
 -- EMPRESA (NOMBRE Y CIF) Y EL IMPORTE TOTAL EN EL AÑO PASADO
 -- Empresa -I- Personas -I- Inscripciones -I- Cursos
-
 SELECT
   Empresas.Nombre,
   Empresas.CIF as NIF,
+  "AAA" as NumeroPedido,
   Sum(Cursos.Importe) as Total
 FROM
   Empresas
